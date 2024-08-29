f2f13ea5fb0ff26d6d17ad47d2e3c38670657c31
Nour Alseaf <nalseaf@andrew.cmu.edu>
2024-08-29 20:23:03 +0300

    fix import and any error by removing require and putting import instead

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 import fibonacci from "./fib"; // Use import instead of require
 
 export default (req: Request, res: Response) => {
-  const { num } = req.params as { num: string }; // Specify num as a string
+  const {num} = req.params as { num: string }; // Specify num as a string
 
   const fibN = fibonacci(parseInt(num, 10)); // Safely parse to an integer
   let result = `fibonacci(${num}) is ${fibN}`;
