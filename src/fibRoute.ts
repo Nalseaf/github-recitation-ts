374ac0ce17281003a70f999ef647177c435fdaeb
Fatou GUEYE <fatougueye@Fatous-MacBook-Air-7.local>
2024-08-30 16:32:35 +0300

    fixed persitent fib issue associated with the parameters

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,7 +1,8 @@
 // Endpoint for querying the fibonacci numbers
 
-import { Request, Response } from "express";
-import fibonacci from "./fib"; // Use import instead of require
+import { Request, Response } from 'express';
+import { fibonacci } from './fib';
+//import fibonacci from "./fib"; // Use import instead of require
 
 export default (req: Request, res: Response) => {
   const {num} = req.params as { num: string }; // Specify num as a string
